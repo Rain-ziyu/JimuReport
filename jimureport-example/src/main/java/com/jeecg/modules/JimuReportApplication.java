5570cd5afabee67840f5ccd91000ec2bf3e2730a
Rain-ziyu <1874300301@qq.com>
2022-06-22 11:06:43 +0800

    修改配置文件

--- jimureport-example/src/main/java/com/jeecg/modules/JimuReportApplication.java
+++ jimureport-example/src/main/java/com/jeecg/modules/JimuReportApplication.java
@@ -10,8 +10,8 @@ import org.springframework.core.env.Environment;
 /**
  * 积木报表独立服务启动类
  */
-@SpringBootApplication
-@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
+@SpringBootApplication(exclude={MongoAutoConfiguration.class})
+
 public class JimuReportApplication {
 
     public static void main(String[] args) {
